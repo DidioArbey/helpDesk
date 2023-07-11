8817330c0836829db041b4e4e4fe2743a4097363
Didio Perdomo <didioarbey@gmail.com>
2023-07-10 21:03:41 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head lang="es">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<title>Login- HelpDesk</title>
+
+	<link href="img/favicon.144x144.png" rel="apple-touch-icon" type="image/png" sizes="144x144">
+	<link href="img/favicon.114x114.png" rel="apple-touch-icon" type="image/png" sizes="114x114">
+	<link href="img/favicon.72x72.png" rel="apple-touch-icon" type="image/png" sizes="72x72">
+	<link href="img/favicon.57x57.png" rel="apple-touch-icon" type="image/png">
+	<link href="img/favicon.png" rel="icon" type="image/png">
+	<link href="img/favicon.ico" rel="shortcut icon">
+
+
+<link rel="stylesheet" href="public/css/separate/pages/login.min.css">
+    <link rel="stylesheet" href="public/css/lib/font-awesome/font-awesome.min.css">
+    <link rel="stylesheet" href="public/css/lib/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="public/css/main.css">
+</head>
+<body>
+
+    <div class="page-center">
+        <div class="page-center-in">
+            <div class="container-fluid">
+                <form class="sign-box">
+                    <div class="sign-avatar">
+                        <img src="public/img/avatar-sign.png" alt="">
+                    </div>
+                    <header class="sign-title">Iniciar Sesion</header>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Correo"/>
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" placeholder="Contraseña"/>
+                    </div>
+                    <div class="form-group">
+                        <div class="checkbox float-left">
+                            <input type="checkbox" id="signed-in"/>
+                            <label for="signed-in">Recordarme</label>
+                        </div>
+                        <div class="float-right reset">
+                            <a href="reset-password.html">Olvide mi contraseña</a>
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-rounded">Ingresar</button>
+                    <p class="sign-note">no tienes cuenta <a href="sign-up.html">Registrate</a></p>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+<script src="public/js/lib/jquery/jquery.min.js"></script>
+<script src="public/js/lib/tether/tether.min.js"></script>
+<script src="public/js/lib/bootstrap/bootstrap.min.js"></script>
+<script src="public/js/plugins.js"></script>
+    <script type="text/javascript" src="public/js/lib/match-height/jquery.matchHeight.min.js"></script>
+    <script>
+        $(function() {
+            $('.page-center').matchHeight({
+                target: $('html')
+            });
+
+            $(window).resize(function(){
+                setTimeout(function(){
+                    $('.page-center').matchHeight({ remove: true });
+                    $('.page-center').matchHeight({
+                        target: $('html')
+                    });
+                },100);
+            });
+        });
+    </script>
+<script src="public/js/app.js"></script>
+</body>
+</html>
